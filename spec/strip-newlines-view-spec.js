15f7fca98552f2dfd141ad866551cd2584bec15b
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2017-11-23 22:26:48 -0500

    Initial commit

---
+++ spec/strip-newlines-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import StripNewlinesView from '../lib/strip-newlines-view';
+
+describe('StripNewlinesView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
