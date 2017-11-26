43d67d2c5ad84917400bfeed84ce395e165e7977
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2017-11-26 05:57:08 -0500

    Removed file dealing with UI elements

--- spec/strip-newlines-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import StripNewlinesView from '../lib/strip-newlines-view';
-
-describe('StripNewlinesView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
